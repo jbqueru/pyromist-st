bb6d82adcced5d66ff6e89002f70b0e6dbe9cc54
Jean-Baptiste Queru <jbqueru@gmail.com>
2020-05-07 21:02:57 -0700

    Update Copyright information

--- pyromist.s
+++ pyromist.s
@@ -1,4 +1,4 @@
-;   Copyright 2020 JBQ
+;   Copyright 2020 Jean-Baptiste M. "JBQ" "Djaybee" Queru
 ;
 ;   Licensed under the Apache License, Version 2.0 (the "License");
 ;   you may not use this file except in compliance with the License.
